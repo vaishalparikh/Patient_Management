e54601244d9dee32d082ee5fa06e161712003720
Arnit Kumar Ghosal <arnitghosal11@gmail.com>
2024-08-06 13:06:19 +0530

    Added Hospital.cs

---
+++ Patient_Management/Hospital.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patient_Management
+{
+    internal class Hospital
+    {
+        public int HospitalId { get; set; }
+        public int HospitalAddress { get; set; }
+        public string HospitalName { get; set; }
+    }
+}
